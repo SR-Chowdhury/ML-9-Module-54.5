573249c32a223baa3938ee9cc92dc5b8293514d4
Shihan Chowdhury <hackedgdown@gmail.com>
2023-04-07 01:28:23 +0100

    Step 4: Handling Not Found Errors done

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -9,7 +9,7 @@ const ErrorPage = () => {
     return (
         <div id="error-page">
             <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
+            <p>Sorry, an unexpected error has occurred. </p>
             <p>
                 <i>{error.statusText || error.message}</i>
             </p>
